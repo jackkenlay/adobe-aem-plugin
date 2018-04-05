ba4bb6e3f914f13f50a99b2f59781f49d1d03b6b
Jack Kenlay <jackk@ensemble.com>
2018-04-05 12:15:34 +0100

    Copied code across

---
+++ src/main/java/SettingsDialog.java
@@ -0,0 +1,23 @@
+import com.intellij.openapi.ui.DialogWrapper;
+
+import javax.annotation.Nullable;
+import javax.swing.*;
+import java.awt.*;
+
+public class SettingsDialog  extends DialogWrapper {
+
+    public SettingsDialog() {
+        super(false);
+        System.out.println("Settings Dialog Loaded");
+        init();
+    }
+
+    @Nullable
+    protected JComponent createCenterPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(new JButton("Button"));
+
+        return panel;
+    }
+}
