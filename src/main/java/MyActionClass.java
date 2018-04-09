59d080956c901ac4dba1f3b7cd21dc3cadcaab1b
Jack Kenlay <jackk@ensemble.com>
2018-04-09 12:07:09 +0100

    Added HTML template

--- src/main/java/MyActionClass.java
+++ src/main/java/MyActionClass.java
@@ -8,8 +8,6 @@ import org.apache.commons.io.FileUtils;
 
 import javax.swing.*;
 import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
 
 public class MyActionClass extends AnAction {
 
@@ -40,8 +38,6 @@ public class MyActionClass extends AnAction {
         //componentName = JOptionPane.showInputDialog(null,"Enter component name:","my-component");
 
 
-
-
         JTextField componentNameInput = new JTextField();
         JTextField componentGroupInput = new JTextField();
 
@@ -58,22 +54,21 @@ public class MyActionClass extends AnAction {
                 "Full Client Libs:",createFullClientLibsChkBx
         };
 
-
-
         //todo set focus to component name input
         int option = JOptionPane.showConfirmDialog(null, message, "Create", JOptionPane.OK_CANCEL_OPTION);
         if (option == JOptionPane.OK_OPTION) {
-            componentName = componentNameInput.getText();
-            componentGroup = componentGroupInput.getText();
-            createClientLibs = createClientLibsChkBx.isSelected();
-            createFullClientLibs = createFullClientLibsChkBx.isSelected();
+            this.componentName = componentNameInput.getText();
+            this.componentGroup = componentGroupInput.getText();
+            this.createClientLibs = createClientLibsChkBx.isSelected();
+            this.createFullClientLibs = createFullClientLibsChkBx.isSelected();
         }
 
         this.currentDir = getCurrentWorkingDirectory(e);
 
 
-        generateCQDialog(componentName);
-
+        generateCQDialog();
+        generateHTML();
+        generateContentXML();
 
         /*---------------------------------------------------
         // create component directory
@@ -106,20 +101,41 @@ public class MyActionClass extends AnAction {
         refreshWindow(e);
     }
 
+    private void generateContentXML() {
+        try {
+            File newFile = this.writeFileFromTemplate("files/content-xml-template.txt",this.currentDir + this.componentName + ".xml");
+            replaceTextInFile(newFile, "componentName",this.componentName);
+            replaceTextInFile(newFile, "componentGroup",this.componentGroup);
+        } catch (Exception e) {
+            //Simple exception handling, replace with what's necessary for your use case!
+            throw new RuntimeException("Generating file failed", e);
+        }
+    }
+
+    private void generateHTML(){
+        try {
+            File newFile = this.writeFileFromTemplate("files/html-template.txt",this.currentDir + this.componentName + ".html");
+            replaceTextInFile(newFile, "componentName",this.componentName);
+        } catch (Exception e) {
+            //Simple exception handling, replace with what's necessary for your use case!
+            throw new RuntimeException("Generating file failed", e);
+        }
+    }
+
     private void refreshWindow(AnActionEvent e){
         Project project = e.getData(PlatformDataKeys.PROJECT);
         project.getBaseDir().refresh(false,true);
     }
 
-    private void createContentXML(String componentName, String componentGroup) {
-        String contentString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<jcr:root xmlns:cq=\"http://www.day.com/jcr/cq/1.0\"\n" +
-                "          xmlns:jcr=\"http://www.jcp.org/jcr/1.0\"\n" +
-                "          jcr:primaryType=\"cq:Component\"\n" +
-                "          jcr:title=\""+componentName+"\"\n" +
-                "          componentGroup=\""+componentGroup+"\"/>";
-        createFile(componentName + "/.content.xml", contentString);
-    }
+//    private void createContentXML(String componentName, String componentGroup) {
+//        String contentString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+//                "<jcr:root xmlns:cq=\"http://www.day.com/jcr/cq/1.0\"\n" +
+//                "          xmlns:jcr=\"http://www.jcp.org/jcr/1.0\"\n" +
+//                "          jcr:primaryType=\"cq:Component\"\n" +
+//                "          jcr:title=\""+componentName+"\"\n" +
+//                "          componentGroup=\""+componentGroup+"\"/>";
+//        createFile(componentName + "/.content.xml", contentString);
+//    }
 
     private void createEditConfig(String componentName) {
         String editConfigText = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
@@ -130,10 +146,10 @@ public class MyActionClass extends AnAction {
         createFile(componentName + "/_cq_editConfig.xml", editConfigText);
     }
 
-    private void createHTML(String componentName) {
-        String htmlText = this.getHTMLText(componentName);
-        createFile(componentName + "/"+componentName+".html", htmlText);
-    }
+//    private void createHTML(String componentName) {
+//        String htmlText = this.getHTMLText(componentName);
+//        createFile(componentName + "/"+componentName+".html", htmlText);
+//    }
 
     private void createFullClientLibs(String componentName, String clientLibCategory) {
         //doesnt make editor LESS OR JS
@@ -277,42 +293,15 @@ public class MyActionClass extends AnAction {
         new File(path).mkdirs();
     }
 
-    private String getHTMLText(String componentName){
-        return "<div id=\"component-"+componentName+"\">\n" +
-               "    ${properties.text || \"Hello\"}\n" +
-               "</div>";
-    }
+//    private String getHTMLText(String componentName){
+//        return "<div id=\"component-"+componentName+"\">\n" +
+//               "    ${properties.text || \"Hello\"}\n" +
+//               "</div>";
+//    }
 
     private String getCurrentWorkingDirectory(AnActionEvent e) {
         VirtualFile file = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-        //VirtualFile folder = file.getParent();
-
-        System.out.println("getCurrentWorkingDirectory returning: "+file.getPath());
         return file.getPath()+"/";
-    }
-
-    private String getCQDialogText(String componentName) {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<jcr:root xmlns:jcr=\"http://www.jcp.org/jcr/1.0\" xmlns:nt=\"http://www.jcp.org/jcr/nt/1.0\"\n" +
-                "	xmlns:sling=\"http://sling.apache.org/jcr/sling/1.0\" jcr:primaryType=\"nt:unstructured\"\n" +
-                "	jcr:title=\""+componentName+"\" sling:resourceType=\"cq/gui/components/authoring/dialog\">\n" +
-                "	<content jcr:primaryType=\"nt:unstructured\"\n" +
-                "		sling:resourceType=\"granite/ui/components/coral/foundation/fixedcolumns\"\n" +
-                "		margin=\"{Boolean}true\">\n" +
-                "		<items jcr:primaryType=\"nt:unstructured\">\n" +
-                "			<column jcr:primaryType=\"nt:unstructured\"\n" +
-                "				sling:resourceType=\"granite/ui/components/coral/foundation/container\">\n" +
-                "				<items jcr:primaryType=\"nt:unstructured\">\n" +
-                "					<text jcr:primaryType=\"nt:unstructured\"\n" +
-                "						sling:resourceType=\"granite/ui/components/coral/foundation/form/textfield\"\n" +
-                "						emptyText=\"My Text\"\n" +
-                "						fieldDescription=\"Add in custom text\"\n" +
-                "						fieldLabel=\"Custom text\" name=\"./text\" />\n" +
-                "				</items>\n" +
-                "			</column>\n" +
-                "		</items>\n" +
-                "	</content>\n" +
-                "</jcr:root>";
     }
 
     private File writeFileFromTemplate(String templateName, String fileName){
@@ -327,7 +316,7 @@ public class MyActionClass extends AnAction {
         return populatedFile;
     }
 
-    private void replaceTextInFile(File file, String word, String replacement){
+    private void replaceTextInFile(File file, String word, String replacement) {
         try {
             FileReader fr = new FileReader(file);
             String s;
@@ -348,95 +337,14 @@ public class MyActionClass extends AnAction {
         }
     }
 
-    private void generateCQDialog(String componentName){
+    private void generateCQDialog() {
         //todo this is hidious - refactor
         try {
-//            InputStream targetStream = this.getClass().getResourceAsStream("files/cq_dialog-template.txt");
-//
-//            File populatedFile = new File(this.currentDir+"test.txt");
-//            FileUtils.copyInputStreamToFile(targetStream, populatedFile);
-
             File newFile = this.writeFileFromTemplate("files/cq_dialog-template.txt",this.currentDir+"test.xml");
-
-            replaceTextInFile(newFile, "componentName",componentName);
-
-//            try {
-//                FileReader fr = new FileReader(newFile);
-//                String s;
-//                String totalStr = "";
-//                try (BufferedReader br = new BufferedReader(fr)) {
-//
-//                    while ((s = br.readLine()) != null) {
-//                        totalStr += s + "\n";
-//                    }
-//
-//                    totalStr = totalStr.replaceAll("componentName", componentName);
-//                    FileWriter fw = new FileWriter(newFile);
-//                    fw.write(totalStr);
-//                    fw.close();
-//                }
-//            } catch(Exception e) {
-//                e.printStackTrace();
-//            }
-
+            replaceTextInFile(newFile, "componentName",this.componentName);
         } catch (Exception e) {
             //Simple exception handling, replace with what's necessary for your use case!
             throw new RuntimeException("Generating file failed", e);
         }
-
-        //cut the dialog to the new directory
-    }
-
-    public void duplicateCQDialogTemplate(){
-        //ClassLoader classLoader = getClass().getClassLoader();
-
-//        URL test = this.getClass().getResource("files/cq_dialog-template.txt");
-//        File testFile = new File(test.toURI());
-
-//        System.out.println("Class:");
-//        System.out.println(this.getClass());
-//        System.out.println("URL: " + test);
-
-
-
-//        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-//        URL itest2 = classloader.getResource("../resources/files/cq_dialog-template.txt");
-//        System.out.println("URL2: " + itest2);
-//
-//        System.out.println("here");
-//        String fileName = "cq_dialog-template.txt";
-//        File file = null;
-//        try{
-//            file = new File(classloader.getResource(fileName).getFile());
-//        }catch (Exception e){
-//            System.out.println("file not found:P " + e.getMessage());
-//        }
-//
-//        System.out.println("here3");
-
-        //File is found
-       // System.out.println("File Found : " + file.exists());
-
-///Users/jackkenlay/personal/adobe-aem-plugin/src/main/resources/files/cq_dialog-template.txt
-        //Read File Content
-        //String content = new String(Files.readAllBytes(file.toPath()));
-        //System.out.println(content);
-
-
-//        File template = new File(classLoader.getResource("/resources/files/cq_dialog-template.txt").getFile());
-//        String tempFilePath = this.currentDir + "/cq_dialog.txt";
-//        File cqdialog = new File(tempFilePath);
-
-        //File cqdialog = new File("cq_dialog.txt");
-//        try {
-//            this.copyFile(template,cqdialog);
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-    }
-
-    private void copyFile(File source, File dest) throws IOException {
-        Files.copy(source.toPath(), dest.toPath());
     }
 }
